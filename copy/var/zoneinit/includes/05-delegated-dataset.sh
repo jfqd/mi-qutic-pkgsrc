4bcb22f4fc348fffdac250036b5d456e25a4e6ee
jfqd <jfqd@blun.org>
2021-12-24 11:27:34 +0100

    add nginx-status endpoint

--- copy/var/zoneinit/includes/05-delegated-dataset.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-UUID=$(mdata-get sdc:uuid)
-DDS=zones/${UUID}/data
-
-if zfs list ${DDS} 1>/dev/null 2>&1; then
-  zfs create ${DDS}/data || true
-  zfs set mountpoint=/data ${DDS}/data
-fi
